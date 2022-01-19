73e08198d15b05d91e20079405d6f02da819b7bc
Ilya <l-zer91@mail.ru>
2022-01-19 18:38:54 +0300

    new commit 19/01

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,19 @@
 package com.company;
 
+import com.company.model.Foot_clubs;
+import com.company.model.Players;
+import com.company.repository.Foot_clubsRepository;
+import com.company.repository.PlayersRepository;
+
 import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        Base.getInstance().addToTable("Savitski", 28);
-        Base.getInstance().updateTable("Putsilooo", 55, 1);
+        Players players = PlayersRepository.getInstance().getById(3);
+        System.out.println(players);
+
+        Foot_clubs foot_clubs = Foot_clubsRepository.getInstance().getById(2);
+        System.out.println(foot_clubs);
     }
 }
